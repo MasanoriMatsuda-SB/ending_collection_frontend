b75d5bd1fdbbbde274445448b9b74e4f24dd0ead
M-Matsuda <masanori.matsuda.sb@gmail.com>
2025-03-18 14:54:18 +0900

    ナビゲーションバーを読みやすく変更。

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
         <title>meme mori</title>
       </head>
       <body>
-        <nav className="p-4 bg-gray-200">
+        <nav className="p-4 bg-gray-200 text-black">
           <Link href="/">Home</Link> |{' '}
           <Link href="/signup">Signup</Link> |{' '}
           <Link href="/login">Login</Link>
