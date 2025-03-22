48e722c7a575ba1c50d530227f0688518aa5a930
ymiki <y.mozuku.m@gmail.com>
2025-03-22 19:27:00 +0900

    開発導線確保のためにナビゲーションに各ページ追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,13 @@ export default function RootLayout({
       <body>
         <nav className="p-4 bg-gray-200 text-black">
           <Link href="/">Home</Link> |{' '}
+          <Link href="/login">Login</Link> |{' '}
           <Link href="/signup">Signup</Link> |{' '}
-          <Link href="/login">Login</Link>
+          <Link href="/signup/finish">SignupFinish</Link> |{' '}
+          <Link href="/grouping">Grouping</Link> |{' '}
+          <Link href="/grouping/finish">GroupingFinish</Link> |{' '}
+          <Link href="/invite">Invite</Link> |{' '}
+          <Link href="/invitation">Invitation</Link>
         </nav>
         <main>{children}</main>
       </body>
